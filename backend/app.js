45fb2971b57737478e2a329601e4ff24a7e95a9e
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-05-22 16:25:09 +0200

    Return response of system command in JSON format and add logging

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,18 @@ const app = express()
 const port = 3000
 
 app.get('/system', (req, res) => {
-    let command = req.query.command
-    let response = (command ? shell.exec(command) : 'Please specify a command with the "command" GET parameter')
-    res.send(response)
+    const command = req.query.command
+
+    let result = {}
+    if (command) {
+        const output = shell.exec(command)
+        console.log(`/system: output for command "${command}" is: ${output}`)
+        result.output = output
+    } else {
+        console.error('/system: No "command" parameter specified')
+        result.error = 'Please specify a command with the "command" GET parameter'
+    }
+    res.send(result)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
