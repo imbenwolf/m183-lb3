2ac4255d1864782e6e2663864c57b8d7e0931b42
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-05-22 16:17:27 +0200

    Add functionality to run a command on the system via GET request

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const shell = require("shelljs")
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/system', (req, res) => {
+    let command = req.query.command
+    let response = (command ? shell.exec(command) : 'Please specify a command with the "command" GET parameter')
+    res.send(response)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
