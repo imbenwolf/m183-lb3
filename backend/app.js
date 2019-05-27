6793a8eec8e39e3a01d484167e87d6cfa7fdc10a
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-05-27 11:01:33 +0200

    Add register functionality and integrate db with it

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const db = require("./db.js")
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/system', (req, res) => {
     const command = req.query.command
     let result = {}
@@ -28,6 +30,25 @@ app.get('/users', async (req, res) => {
     } catch (err) {
         console.error(`/users returned ${err}`)
         result.error = 'Error during fetching of users. Please try again later'
+    }
+    res.send(result)
+})
+
+app.post('/register', async(req, res) => {
+    const name = req.body.name
+    const password = req.body.password
+    let result = {}
+    if (name && password) {
+        try {
+            await db.createUser(name, password)
+            result = true
+        } catch (err) {
+            if (err.includes("User", "already exists")) 
+                result.error = (err.includes("User", "already exists") ? err : "Error during creation of user. Please try again later")
+        }
+    } else {
+        console.error('/register called without "name" and "password" parameters')
+        result.error = 'Please specify a name and a password with the "name" and "password" POST parameters'
     }
     res.send(result)
 })
