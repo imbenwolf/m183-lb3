7f7729ca437302d0a03d626585f0feb06649cb9f
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-05-27 10:55:25 +0200

    Add sqlite3 db for users with setup function

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(':memory:')
+
+const setup = () => new Promise((resolve, reject) =>
+    db.serialize(async () => {
+        try {
+            db.run('CREATE TABLE user (name TEXT, password TEXT)', err => 
+                err ? reject(err) : err
+            )
+            db.run('INSERT INTO user VALUES (?, ?)', "123", "sml12345")
+            db.run('INSERT INTO user VALUES (?, ?)', "lb3", "sml12345")
+            resolve()
+        } catch (err) {
+            reject(err)
+        }
+    })
+)
+
+module.exports = {
+    setup
+}
