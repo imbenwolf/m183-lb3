2fc34ad1d6895e74264c98703e9460f7e545a8b8
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-05-31 16:35:22 +0200

    Add auth module to handle registration of users

---
+++ backend/auth.js
@@ -0,0 +1,25 @@
+const db = require("./db.js")
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const setup = async () => {
+    await register("123", "sml12345")
+    await register("lb3", "sml12345")
+}
+
+const register = async (name, password) => {
+    if (name && password) {
+        const user = await db.getUser(name)
+        if (user) throw `user "${name}" already exists`
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
+        await db.createUser(name, hashedPassword)
+    } else {
+        throw 'name and password must be supplied'
+    }
+}
+
+module.exports = {
+    setup,
+    register
+}
