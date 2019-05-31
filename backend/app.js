aaca18bda6c20b526c9f86bec7ef71d5c5b298ff
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-05-31 16:36:13 +0200

    Integrate auth module with app and update the register endpoint

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan")
 const shell = require("shelljs")
 
 const db = require("./db.js")
+const auth = require("./auth.js")
 
 const app = express()
 const port = 3000
@@ -12,22 +13,26 @@ app.use(express.urlencoded({extended: true}))
 
 app.get('/system', (req, res) => {
     const command = req.query.command
+
     let result = {}
     if (command) {
         result.output = shell.exec(command, { silent: true })
     } else {
         result.error = 'Please specify a command with the "command" GET parameter'
     }
+
     res.send(result)
 })
 
 app.get('/users', async (req, res) => {
     let result = {}
+
     try {
         result = await db.getUsers()
     } catch (err) {
         result.error = 'Error during fetching of users. Please try again later'
     }
+
     res.send(result)
 })
 
@@ -35,24 +40,23 @@ app.post('/register', async(req, res) => {
     const name = req.body.name
     const password = req.body.password
     let result = {}
-    if (name && password) {
-        try {
-            await db.createUser(name, password)
-            result = true
-        } catch (err) {
-            let userExists = err.includes("User", "already exists")
-            result.error = (userExists) ? err : "Error during creation of user. Please try again later"
-        }
-    } else {
-        result.error = 'Please specify a name and a password with the "name" and "password" POST parameters'
+
+    try {
+        await auth.register(name, password)
+        result.success = true
+    } catch (err) {
+        result.error = (typeof err === 'string') ? err : "Error during creation of user. Please try again later"
     }
+
     res.send(result)
 })
 
 const server = app.listen(port, async () => {
     try {
         await db.setup()
-        console.log(`LB3 backend running on port: ${port}!`)
+        await auth.setup()
+
+        console.log(`lb3 running on port: ${port}!`)
     } catch (err) {
         console.error(`setup returned ${err}`)
         server.close()
